88ea1dec3350b8fe6c3ce4872cd995c30c54ace4
Ethan Leisinger <packruler@gmail.com>
2022-06-08 02:05:12 +0000

    temp test

--- config_test.go
+++ config_test.go
@@ -1,31 +1,31 @@
 package rewrite_body
 
-// import (
-// 	"log"
-// 	"reflect"
-// 	"testing"
+import (
+	"log"
+	"reflect"
+	"testing"
 
-// 	"github.com/BurntSushi/toml"
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/BurntSushi/toml"
+	"github.com/stretchr/testify/assert"
+)
 
-// type testStruct struct {
-// 	Config Config `json:"config,omitempty" toml:"config,omitempty" yaml:"config,omitempty"`
-// }
+type testStruct struct {
+	Config Config `json:"config,omitempty" toml:"config,omitempty" yaml:"config,omitempty"`
+}
 
-// func TestConfigParsing(t *testing.T) {
-// 	cfg := &testStruct{}
+func TestConfigParsing(t *testing.T) {
+	cfg := &testStruct{}
 
-// 	_, err := toml.DecodeFile("./fixtures/sample.toml", &cfg)
-// 	if err != nil {
-// 		t.Errorf("Unable to decode file: %v", err)
-// 	}
+	_, err := toml.DecodeFile("./fixtures/sample.toml", &cfg)
+	if err != nil {
+		t.Errorf("Unable to decode file: %v", err)
+	}
 
-// 	reflection := reflect.ValueOf(cfg)
-// 	log.Println("Decoded: ", reflection)
+	reflection := reflect.ValueOf(cfg)
+	log.Println("Decoded: ", reflection)
 
-// 	cfgCopy := cfg
-// 	assert.Equal(t, reflect.ValueOf(cfgCopy), reflect.ValueOf(cfg))
-// 	assert.Equal(t, reflect.ValueOf(cfgCopy), reflect.ValueOf(cfg))
-// 	assert.Equal(t, cfgCopy, cfg)
-// }
+	cfgCopy := cfg
+	assert.Equal(t, reflect.ValueOf(cfgCopy), reflect.ValueOf(cfg))
+	assert.Equal(t, reflect.ValueOf(cfgCopy), reflect.ValueOf(cfg))
+	assert.Equal(t, cfgCopy, cfg)
+}
--- go.mod
+++ go.mod
@@ -2,3 +2,7 @@ module github.com/packruler/rewrite-body
 
 go 1.16
 
+require (
+	github.com/BurntSushi/toml v1.1.0
+	github.com/stretchr/testify v1.7.2
+)
